53e69c2ab9d214db905850f9a8aebfaf01380953
rahul <rahul.manda@supernal-it.com>
2021-07-13 13:10:00 +0530

    Log message on app launch Button handled message

--- GitUseExample/ViewController.swift
+++ GitUseExample/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .white
+        
+       let button = UIButton()
+        button.setTitle("Just A Button", for: .normal)
+        button.setTitleColor(.blue, for: .normal)
+        button.addTarget(self, action: #selector(handleButton), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(button)
+        button.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        button.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
     }
 
+    @objc func handleButton(){
+        print("This is a Git tutorial")
 
+    }
+    
 }
 
